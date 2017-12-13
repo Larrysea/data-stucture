4c24d4bce45b6895b3d20c9a72e0fbacea0fa1b0
larrysea <lzh1994610@163.com>
2017-12-14 00:22:38 +0800

    对称字符串查找，冒泡排序。

--- src/NowCoder/BubleSort.java
+++ src/NowCoder/BubleSort.java
@@ -1,7 +1,38 @@
 package NowCoder;
 
+import common_util.CommonUtil;
+
 /**
  * Created by Larry-sea on 2017/12/13.
+ * 冒泡排序
  */
 public class BubleSort {
+
+    public static void main(String[] args) {
+
+
+        int[] array = {1, 3, 2, 5, 6, 9};
+        CommonUtil.printArray(bubbleSort(array, 6));
+    }
+
+    /*
+    * 冒泡排序算法思想，每一次将该数组中最大的数字排在最后面
+    * 通过n^2 次筛选最终排出该数组
+    *
+    * */
+    public static int[] bubbleSort(int[] A, int n) {
+        // write code here
+        for (int index = n - 1; index > 0; index--) {
+            for (int j = 0; j < index; j++) {
+                if (A[j] > A[j + 1]) {
+                    int temp = A[j + 1];
+                    A[j + 1] = A[j];
+                    A[j] = temp;
+                }
+            }
+        }
+        return A;
+    }
+
+
 }
--- src/NowCoder/Rotation.java
+++ src/NowCoder/Rotation.java
@@ -4,4 +4,34 @@ package NowCoder;
  * Created by Larry-sea on 2017/12/12.
  */
 public class Rotation {
+    /*
+    * 代码逻辑：
+    * 1 判断两个字符串为不为空，长度是不是合理范围
+    * 2 判断两个字符串是否长度是否相等
+    * 3 构建两个字符串拼接好的字符
+    * 4 拼接而成的字符串拼接看是否相等，如果相等则判断是否包含
+    * 5 如果包含则返回true，否则返回false
+    *
+    *
+    *
+    * */
+
+
+    public static boolean chkRotation(String A, int lena, String B, int lenb) {
+        // write code here
+        if (A == null || B == null || lena <= 0 || lenb <= 0) {
+            return false;
+        }
+        String doubleString = A + A;
+        return doubleString.contains(B);
+
+    }
+
+    public static void main(String[] args) {
+        boolean result1 = chkRotation("", 6, "", 5);
+        boolean result2 = chkRotation("cdab", 4, "abcd", 4);
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
 }

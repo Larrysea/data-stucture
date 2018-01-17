6d3cee9a311bb05e417cc23c925743a8296158c0
larrysea <lzh1994610@163.com>
2018-01-17 23:48:36 +0800

    字符串查找解法。

--- src/data_stucture/PrintUtil.java
+++ src/data_stucture/PrintUtil.java
@@ -1,9 +1,12 @@
 package data_stucture;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Larry-sea on 2017/10/14.
  */
-public class PrintUtil {
+public class PrintUtil{
 
     public static void println(String params) {
         System.out.println(params);
@@ -16,6 +19,39 @@ public class PrintUtil {
     public static void print(int params)
     {
         System.out.println(params);
+    }
+
+
+
+    public static void main(String [] args)
+    {
+        List<List<Integer>> result=new ArrayList<List<Integer>>();
+        List<Integer> child=new ArrayList();
+        child.add(1);
+        child.add(2);
+        child.add(3);
+        result.add(child);
+        List<Integer> child1=new ArrayList<>();
+        child1.add(4);
+        child1.add(5);
+        child1.add(6);
+        result.add(child1);
+        print(result);
+    }
+
+
+    public static void print(List<List<Integer>> integerList)
+    {
+        List<String> result=new ArrayList<String>();
+        for(int i=0;i<integerList.size();i++)
+        {
+            result.add(integerList.get(i).toString());
+        }
+        for(String params:result)
+        {
+            System.out.println(params);
+        }
+
     }
 }
 
--- src/ninechapter/StringContain.java
+++ src/ninechapter/StringContain.java
@@ -3,7 +3,6 @@ package ninechapter;
 import data_stucture.PrintUtil;
 
 
-
 //lintcode 13 题
 public class StringContain {
 
@@ -14,12 +13,11 @@ public class StringContain {
      * @return: a index to the first occurrence of target in source, or -1  if target is not part of source.
      */
     private static int strStr(String source, String target) {
-        if (source == null || target == null ) {
+        if (source == null || target == null) {
             return -1;
         }
-        if(target.length() == 0 && source.length() == 0)
-        {
-           return 0;
+        if (target.length() == 0 && source.length() == 0) {
+            return 0;
         }
         if (target.length() > source.length()) {
             return -1;
@@ -29,7 +27,81 @@ public class StringContain {
 
     public static void main(String[] args) {
         strStr(null, null);
-        PrintUtil.print(strStr("fasdfas", ""));
+        PrintUtil.print(strStrII("fasdfas", ""));
+        PrintUtil.print(strStrII("fasdfas", "f"));
+
+        int i=0;
+        for( i=0;i<10;i++)
+        {
+
+        }
+
+        System.out.println("i"+i);
     }
+
+
+    private static int strStrII(String source, String target) {
+        if (source == null || target == null) {
+            return -1;
+        }
+        if (target.length() == 0 && source.length() == 0) {
+            return 0;
+        }
+        if (target.length() > source.length()) {
+            return -1;
+        }
+        if (target.length() == 0) {
+            return 0;
+        }
+        for (int i = 0; i < source.length(); i++) {
+            if (source.charAt(i) == target.charAt(0)) {
+                int sourceIndex = i;
+                if (target.length() > 1) {
+                    for (int j = 0; j < target.length(); ) {
+                        if (source.charAt(sourceIndex) == target.charAt(j)) {
+                            sourceIndex++;
+                            j++;
+                        } else {
+                            break;
+                        }
+                        if (j == target.length() - 1) {
+                            return i;
+                        }
+                    }
+                } else {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    private static int strStrIII(String source,String target)
+    {
+        if(source==null||target==null)
+        {
+            return -1;
+        }
+        for(int i=0;i<source.length()-target.length()+1;i++)
+        {
+            int j=0;
+            for(;j<target.length();j++)
+            {
+                if(source.charAt(i+j)!=target.charAt(j))
+                {
+                    break;
+                }
+            }
+            if(j==target.length())
+            {
+                return i;
+            }
+
+        }
+        return -1;
+
+    }
+
 
 }

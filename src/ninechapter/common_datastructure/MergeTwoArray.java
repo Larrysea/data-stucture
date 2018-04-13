cdfc942170b50dfb54dbae08628af6b42503418a
larrysea <lzh1994610@163.com>
2018-04-13 23:42:38 +0800

    双指针法解决。

--- src/ninechapter/common_datastructure/MergeTwoArray.java
+++ src/ninechapter/common_datastructure/MergeTwoArray.java
@@ -14,13 +14,13 @@ public class MergeTwoArray {
 
 
     public static void main(String[] args) {
-        int[] test1 = {1, 2, 3, -1, -1, -1};
-        int[] test2 = {4, 5};
-        mergeSortedArray(test1, 3, test2, 2);
+        int[] test1 = {1, 7, 9, -1, -1, -1};
+        int[] test2 = {4, 8};
+        mergeSortedArrayII(test1, 3, test2, 2);
         PrintUtil.printArray(test1);
     }
 
-
+    //调用系统排序方法，不够好
     public static void mergeSortedArray(int[] A, int m, int[] B, int n) {
         // write your code here
         int startIndex = m;
@@ -30,6 +30,32 @@ public class MergeTwoArray {
             A[startIndex++] = B[count++];
         }
         Arrays.sort(A);
+    }
+
+    //通过两个指针的解决方法
+    public static void mergeSortedArrayII(int[] A, int m, int[] B, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int index = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (A[i] > B[j]) {
+                A[index--] = A[i--];
+            } else {
+                A[index--] = B[j--];
+            }
+        }
+
+        //如果还有剩下的一方
+        while (i >= 0) {
+            A[index--] = A[i--];
+        }
+
+        while (j >= 0) {
+            A[index--] = B[j--];
+        }
 
     }
+
+
 }
